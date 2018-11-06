46cc6b00c22de1e21ca82513c5a0ce3eb70645ec
Bartomeu <bartomeugm@gmail.com>
2018-11-06 11:00:31 +0100

    implementation with order

--- src/test/java/CopierShould.java
+++ src/test/java/CopierShould.java
@@ -1,32 +1,24 @@
 import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
+import org.mockito.Mockito;
 
-import java.util.ArrayList;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.when;
 
 public class CopierShould {
 
     @Test
     void call_getChar_same_times_as_setChar() {
-        ArrayList<Supplier<Character>> sourceLambdas = new ArrayList<>();
-        sourceLambdas.add(() -> 'a');
-        sourceLambdas.add(() -> 'b');
-        sourceLambdas.add(() -> '\n');
-        SourceSpy source = new SourceSpy(sourceLambdas);
-
-        ArrayList<Consumer<Character>> destinationLambdas = new ArrayList<>();
-        destinationLambdas.add((Character inputCharacter) -> {});
-        destinationLambdas.add((Character inputCharacter) -> {});
-        destinationLambdas.add((Character inputCharacter) -> {});
-        DestinationSpy destination = new DestinationSpy(destinationLambdas);
+        ISource source = Mockito.mock(ISource.class);
+        IDestination destination = Mockito.mock(IDestination.class);
+        when(source.getChar()).thenReturn('a', 'b', '\n');
 
         Copier copier = new Copier(source, destination);
-
         copier.copy();
 
-        assertTrue(source.verifyGetCharCalledNTimes(3));
-        assertTrue(destination.verifySetCharCalledNTimes(3));
+        InOrder order = inOrder(source, destination);
+        order.verify(destination).setChar('a');
+        order.verify(destination).setChar('b');
+        order.verify(destination).setChar('\n');
     }
 }
